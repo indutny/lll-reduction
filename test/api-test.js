e0ff745f047f00713cfa09fcb910ed27215a3c18
Fedor Indutny <fedor@indutny.com>
2014-06-17 22:02:36 -0700

    lib: use fract.js

--- test/api-test.js
+++ test/api-test.js
@@ -1,15 +1,15 @@
 var assert = require('assert');
 
 var lll = require('../');
-var bn = require('bn.js');
+var F = require('fract.js');
 
 describe('LLL', function() {
   describe('Gram-Schmidt process', function() {
     it('should make 2d-basis orthogonal', function() {
       var l = new lll();
       var basis = l.gramSchmidt([
-        [ new bn(3), new bn(1) ],
-        [ new bn(2), new bn(2) ]
+        [ new F(3), new F(1) ],
+        [ new F(2), new F(2) ]
       ]).basis;
 
       assert.equal(basis[0].dot(basis[1]).cmpn(0), 0);
@@ -18,9 +18,9 @@ describe('LLL', function() {
     it('should make 3d-basis orthogonal', function() {
       var l = new lll();
       var basis = l.gramSchmidt([
-        [ new bn(3), new bn(2), new bn(1) ],
-        [ new bn(1), new bn(3), new bn(2) ],
-        [ new bn(2), new bn(1), new bn(3) ],
+        [ new F(3), new F(2), new F(1) ],
+        [ new F(1), new F(3), new F(2) ],
+        [ new F(2), new F(1), new F(3) ],
       ]).basis;
 
       assert.equal(basis[0].dot(basis[1]).cmpn(0), 0);
@@ -33,26 +33,10 @@ describe('LLL', function() {
     it('should reduce 3d-basis', function() {
       var l = new lll();
       var basis = l.reduce([
-        [ new bn(1), new bn(1), new bn(1) ],
-        [ new bn(-1), new bn(0), new bn(2) ],
-        [ new bn(3), new bn(5), new bn(6) ]
-      ], new bn(3), new bn(4));
-
-      var b = basis.map(function(b) {
-        return b.inspect();
-      }).join(', ');
-      assert.equal(b, '<Vec: <Frac: 0/1>, <Frac: 1/1>, <Frac: 0/1>>, ' +
-                      '<Vec: <Frac: 1/1>, <Frac: 0/1>, <Frac: 1/1>>, ' +
-                      '<Vec: <Frac: -2/1>, <Frac: 0/1>, <Frac: 1/1>>');
-    });
-
-    it('should reduce 3d-basis', function() {
-      var l = new lll();
-      var basis = l.reduce([
-        [ new bn(1), new bn(1), new bn(1) ],
-        [ new bn(-1), new bn(0), new bn(2) ],
-        [ new bn(3), new bn(5), new bn(6) ]
-      ], new bn(3), new bn(4));
+        [ new F(1), new F(1), new F(1) ],
+        [ new F(-1), new F(0), new F(2) ],
+        [ new F(3), new F(5), new F(6) ]
+      ], new F(3, 4));
 
       var b = basis.map(function(b) {
         return b.inspect();
